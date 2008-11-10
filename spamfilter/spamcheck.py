e6d8a935fa85a560cd486758a8fda72bc6f72474
David J. Mellor <dmellor@whistlingcat.com>
2008-11-09 16:30:08 -0800

    Check if the HELO string is whitelisted even if the sender is null.

--- spamfilter/spamcheck.py
+++ spamfilter/spamcheck.py
@@ -37,15 +37,16 @@ class SpamCheck(SmtpProxy, ConfigMixin):
         # checks. This is to prevent quarantining mail that SpamAssassin
         # consistently and incorrectly flags as spam.
         whitelist_db = self.getConfigItem('spamfilter', 'whitelist_db', None)
-        if whitelist_db and self.mail_from:
-            # First check the full address.
-            if queryPostfixDB(whitelist_db, self.mail_from):
-                return True
+        if whitelist_db:
+            if self.mail_from:
+                # First check the full address.
+                if queryPostfixDB(whitelist_db, self.mail_from):
+                    return True
 
-            # Check the domain.
-            domain = self.mail_from.index('@') + 1
-            if queryPostfixDB(whitelist_db, self.mail_from[domain:]):
-                return True
+                # Check the domain.
+                domain = self.mail_from.index('@') + 1
+                if queryPostfixDB(whitelist_db, self.mail_from[domain:]):
+                    return True
 
             # Check if the HELO string is whitelisted.
             if queryPostfixDB(whitelist_db, self.remote_host):
