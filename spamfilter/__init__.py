73eaa704ad59175171e692d213b1162d174ed1c9
David J. Mellor <dmellor@whistlingcat.com>
2009-04-18 14:06:16 -0700

    Updated the logging messages to include the process id of the policy daemon.

--- spamfilter/__init__.py
+++ spamfilter/__init__.py
@@ -2,7 +2,7 @@ import logging
 from logging.handlers import SysLogHandler
 
 handler = SysLogHandler('/dev/log', SysLogHandler.LOG_MAIL)
-handler.setFormatter(logging.Formatter('%(message)s'))
+handler.setFormatter(logging.Formatter('policy[%(process)d]: %(message)s'))
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 root.addHandler(handler)
