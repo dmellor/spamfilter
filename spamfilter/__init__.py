5bef451db153c5069ccf72ff98bb818a128bad8a
David J. Mellor <dmellor@whistlingcat.com>
2009-04-27 17:21:07 -0700

    Customise the formatting of log messages.
    The format begins with the name of the script being executed, which is passed
    to the spamfilter module via sys.argv[0].

--- spamfilter/__init__.py
+++ spamfilter/__init__.py
@@ -1,8 +1,10 @@
+import sys
 import logging
 from logging.handlers import SysLogHandler
 
 handler = SysLogHandler('/dev/log', SysLogHandler.LOG_MAIL)
-handler.setFormatter(logging.Formatter('policy[%(process)d]: %(message)s'))
+handler.setFormatter(
+    logging.Formatter(sys.argv[0] + '[%(process)d]: %(message)s'))
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 root.addHandler(handler)
