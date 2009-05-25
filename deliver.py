a0f7dd1158a253d7dfe056b3b718c921f99b4086
David J. Mellor <dmellor@whistlingcat.com>
2009-05-25 15:01:13 -0700

    Updated the delivery CGI script to use SQLAlchemy instead of psycopg2.

--- deliver.py
+++ deliver.py
@@ -1,76 +1,73 @@
-#!/usr/bin/python2.5
-
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_SERIALIZABLE
-import smtplib
+#!/usr/bin/python2.6
 import os
 import sys
+import site
+import smtplib
 
-database = "spamassassin"
-user = "qmail"
-password = "38hb75"
+deployment_dir = sys.path[0]
+site.addsitedir('%s/lib/python2.6/site-packages' % deployment_dir)
 
-def main():
-    method = os.getenv("REQUEST_METHOD")
-    if method == "GET":
-        _confirm("http://" + os.getenv("SERVER_NAME") +
-                 os.getenv("SCRIPT_NAME") + os.getenv("PATH_INFO"))
-    elif method != "POST":
-        sys.exit()
-        
-    pathInfo = os.getenv("PATH_INFO")
-    if pathInfo[0] != "/":
-        _invalidId()
-        
-    deliveryId = pathInfo[1:]
-    connection = psycopg2.connect(database=database, user=user,
-                                  password=password)
+from spamfilter.mixin import *
+from spamfilter.model.spam import Spam, SpamRecipient
+from sqlalchemy import text
 
-    connection.set_isolation_level(ISOLATION_LEVEL_SERIALIZABLE)
-    cursor = connection.cursor()
-    try:
-        cursor.execute(
-            """SELECT recipient, spam_id FROM spam_recipients
-            WHERE delivery_id = %s""",
-            [deliveryId])
-        if cursor.rowcount == 0:
+class Deliver(ConfigMixin):
+    def __init__(self):
+        self.readConfig(os.path.join(deployment_dir, 'config.ini'))
+
+    def process(self):
+        method = os.getenv('REQUEST_METHOD')
+        if method == 'GET':
+            _confirm('http://' + os.getenv('SERVER_NAME') +
+                     os.getenv('SCRIPT_NAME') + os.getenv('PATH_INFO'))
+        elif method != 'POST':
+            sys.exit()
+
+        path_info = os.getenv('PATH_INFO')
+        if path_info[0] != '/':
             _invalidId()
-            
-        recipient, savedMailId = cursor.fetchone()
 
-        # Determine the real recipient.
-        cursor.execute(
-            "SELECT delivery FROM quarantine_recipients WHERE %s ~ regexp",
-            [recipient])
-        if cursor.rowcount != 0:
-            recipient = cursor.fetchone()[0]
+        delivery_id = path_info[1:]
+        session = createSession(self.getConfigItem('database', 'dburi'))
+        try:
+            query = session.query(SpamRecipient).filter_by(
+                delivery_id=delivery_id)
+            spam_recipient = query.first()
+            if not spam_recipient:
+                _invalidId()
 
-        cursor.execute(
-             "SELECT mail_from, contents FROM spam WHERE id = %s",
-             [savedMailId])
-        mailFrom, msg = cursor.fetchone()
-        
-        # Deliver the message.
-        mailServer = smtplib.SMTP("localhost")
-        mailServer.sendmail(mailFrom, recipient, msg, ["BODY=8BITMIME"])
-        mailServer.quit()
+            # Determine the real recipient.
+            connection = session.connection()
+            statement = text(
+                'SELECT delivery FROM quarantine_recipients WHERE :x ~ regexp')
+            result = connection.execute(
+                statement, x=spam_recipient.recipient).fetchone()
+            if result:
+                recipient = result[0]
+            else:
+                recipient = spam_recipient.recipient
 
-        cursor.execute(
-            "DELETE FROM spam_recipients WHERE delivery_id = %s",
-            [deliveryId])
-        cursor.execute(
-            """SELECT COUNT(*) FROM spam_recipients WHERE spam_id = %s""",
-            [savedMailId])
-        number = cursor.fetchone()[0]
-        if number == 0:
-            cursor.execute("DELETE FROM spam WHERE id = %s", [savedMailId])
-        
-        connection.commit()
-        _success()
-    except:
-        connection.rollback()
-        raise
-    
+            spam = session.query(Spam).get(spam_recipient.spam_id)
+
+            # Deliver the message.
+            mailServer = smtplib.SMTP('localhost')
+            mailServer.sendmail(spam.mail_from, recipient, spam.contents,
+                                ['BODY=8BITMIME'])
+            mailServer.quit()
+
+            # Delete the entry in the spam_recipients table and the message if
+            # the number of spam recipients has dropped to zero.
+            session.delete(spam_recipient)
+            query = session.query(SpamRecipient)
+            if query.filter_by(spam_id=spam_recipient.spam_id).count() == 0:
+                session.delete(spam)
+
+            session.commit()
+            _success()
+        except:
+            session.rollback()
+            raise
+
 def _invalidId():
     print "Content-Type: text/html"
     print
@@ -99,4 +96,4 @@ def _confirm(url):
     sys.exit()
 
 if __name__ == "__main__":
-    main()
+    Deliver().process()
