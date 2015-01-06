dmellor/spamfilter

commit 6110ed2b629802fb52caaf1cc2e0a09935306c48

Fixed warnings reported by PyCharm.

spamfilter/netcmd.py
@@ -25,7 +25,7 @@ class NetCommand(object):
     SMTP.
     """
 
-    def __init__(self, fp):
+    def __init__(self, fp=None):
         """
         Constructor for a NetCommand obect. The fp argument should be a
         file-like object that supports two methods:
@@ -35,7 +35,8 @@ class NetCommand(object):
 
         The Python file and socket objects are compatible with this class.
         """
-        self.open(fp)
+        if not fp:
+            self.open(fp)
 
     def open(self, fp):
         self.fp = fp
@@ -63,7 +64,6 @@ class NetCommand(object):
             self.net_cmd_debug = level
 
         return oldval
-
     def debugText(self, out, text):
         """
         This method is called to print debugging information. text is the text
@@ -138,11 +138,11 @@ class NetCommand(object):
             cmd += '\015\012'
 
             try:
-                bytes = os.write(fd, cmd)
+                cmd_bytes = os.write(fd, cmd)
             except:
-                bytes = None
+                cmd_bytes = None
 
-            if bytes is None or bytes != len(cmd):
+            if cmd_bytes is None or cmd_bytes != len(cmd):
                 try:
                     self.close()
                 except:
@@ -162,7 +162,7 @@ class NetCommand(object):
         not hold may override this method.
         """
         code = int(self.code())
-        return code > 0 and code < 400
+        return 0 < code < 400
 
     def unsupported(self):
         """
@@ -200,7 +200,7 @@ class NetCommand(object):
                     return None
 
                 # Prepend the last data read and then break into lines.
-                buf = partial + buf
+                buf += partial
                 buf = re.split(r'\015?\012', buf)
                 partial = buf.pop(len(buf) - 1)
                 self.net_cmd_lines.extend([x + '\n' for x in buf])

deliver.py
@@ -7,7 +7,6 @@ from spamfilter.mixin import *
 from spamfilter.model.spam import Spam, SpamRecipient
 from spamfilter.model.autowhitelist import AutoWhitelist
 from spamfilter.report import translate
-from sqlalchemy import text
 
 class Deliver(ConfigMixin):
     def __init__(self):
@@ -143,6 +142,7 @@ def getBodyTypeCharset(message):
     payload = message.get_payload()
     content_type = message.get_content_type()
     charset = message.get_param('charset')
+    body = None
     if not isinstance(payload, list):
         if content_type in ('text/plain', 'text/html'):
             body = payload
@@ -154,8 +154,6 @@ def getBodyTypeCharset(message):
             elif transfer_encoding == 'base64':
                 import base64
                 body = base64.b64decode(body)
-        else:
-            body = None
     else:
         for msg in payload:
             body, content_type, charset = getBodyTypeCharset(msg)

spamfilter/bounce.py
@@ -1,4 +1,5 @@
-import sys, re
+import sys
+import re
 import email
 from email.mime.text import MIMEText
 from email.mime.message import MIMEMessage
@@ -58,7 +59,7 @@ def generate_bounce(spam, sender):
     report['Content-Description'] = 'Delivery report'
     contents = MIMEMessage(spam)
     contents['Content-Description'] = 'Undelivered Message'
-    _params={'report-type': 'delivery-status'}
+    _params = {'report-type': 'delivery-status'}
     message = MIMEMultipart('report', **_params)
     message.attach(notification)
     message.attach(report)
@@ -104,6 +105,7 @@ class FileBouncer(EmailExtractor):
         bounce(message)
 
 from spamfilter.mixin import *
+
 class DbBouncer(ConfigMixin):
     def __init__(self, config):
         self.readConfig(config)
@@ -111,6 +113,7 @@ class DbBouncer(ConfigMixin):
 
     def bounceFromId(self, spam_id):
         from spamfilter.model.spam import Spam
+
         spam = self.session.query(Spam).get(spam_id)
         spam_message = email.message_from_string(spam.contents)
         bounce(spam_message, spam.bounce)

spamfilter/extract.py
@@ -1,4 +1,3 @@
-import sys
 import email
 import base64
 import quopri

spamfilter/policy.py
@@ -55,6 +55,7 @@ class PolicyManager(ConfigMixin):
                 self.values = {}
 
     def determineAction(self):
+        action = err_status = None
         retries = 0
         num_retries = int(self.getConfigItem('general', 'retries', 2))
         while retries < num_retries:

spamfilter/relay.py
@@ -65,6 +65,7 @@ class Relay(SmtpProxy, ConfigMixin):
             # in which case we send the XCLIENT command, discard the response
             # and then send the synthesised HELO command and discard its
             # response before issuing the mail command.
+            name = None
             send_commands = True
             pop_db = self.getConfigItem('spamfilter', 'pop_db', None)
             if pop_db and queryPostfixDB(pop_db, self.remote_addr):

spamfilter/report.py
@@ -10,8 +10,8 @@ import codecs
 from email.header import decode_header
 
 from spamfilter.mixin import ConfigMixin, createSession
-from spamfilter.model.spam import Spam, SpamRecipient, spam_recipients_table
-from spamfilter.model.virus import Virus, VirusRecipient, virus_recipients_table
+from spamfilter.model.spam import SpamRecipient, spam_recipients_table
+from spamfilter.model.virus import VirusRecipient, virus_recipients_table
 from mako.template import Template
 
 class MessageSummary(object):

spamfilter/smtpproxy.py
@@ -21,8 +21,10 @@ class SmtpProxy(netcmd.NetCommand):
     to quarantine the message if it is identified as spam or if it contains a
     virus.
     """
-    def __init__(self, input=sys.stdin, output=sys.stdout, host='localhost',
-                 port=25):
+
+    def __init__(self, smtp_input=sys.stdin, smtp_output=sys.stdout,
+                 host='localhost', port=25):
+        super(SmtpProxy, self).__init__()
         self.host = host
         self.port = port
         self.rcpt_to = []
@@ -30,14 +32,11 @@ class SmtpProxy(netcmd.NetCommand):
         self.remote_addr = None
         self.remote_host = None
         self.error_response = None
-        self.input = input
-        self.output = output
 
         # Open a connection to the remote server.
+        self.input = smtp_input
+        self.output = smtp_output
         self.openConnection()
-
-        # Send the initial greeting response from the second server to the
-        # first server.
         self.sendResponse()
 
     def openConnection(self):
@@ -110,6 +109,7 @@ class SmtpProxy(netcmd.NetCommand):
         # replaces the \r\n sequences at the end of the line with a bare
         # linefeed.
         message = self.message()
+
         def reformat(line):
             line = line.rstrip('\n')
             return '-%s\r\n' % line

commit 2f1e18e34ef609fbb8615abc4429a985d434852f

Fixed low-level line reading bug introduced in 6110ed2.

spamfilter/netcmd.py
@@ -201,7 +201,7 @@ class NetCommand(object):
                     return None
 
                 # Prepend the last data read and then break into lines.
-                buf += partial
+                buf = partial + buf
                 buf = re.split(r'\015?\012', buf)
                 partial = buf.pop(len(buf) - 1)
                 self.net_cmd_lines.extend([x + '\n' for x in buf])