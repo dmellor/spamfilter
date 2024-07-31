24ef146e6736673aa82481a636b68e6ea46c7f9f
David Mellor <dmellor@whistlingcat.com>
2024-07-31 16:40:09 -0700

    Fix error in email extraction
    If an attachment has the quoted-printable content type, Python's email parser
    does not always correctly parse the payload extracted from the email object so
    we have to extract the raw content, convert it and then genearate a new email
    object.

--- spamfilter/extract.py
+++ spamfilter/extract.py
@@ -2,79 +2,60 @@ import email
 import base64
 import quopri
 import re
+import sys
 
 
 class EmailExtractor(object):
     # noinspection PyAttributeOutsideInit
     def process(self, fp):
-        self.original_message = ''.join(fp.readlines())
-        self.processed = False
-        self.process_message(email.message_from_string(self.original_message))
+        raw = re.sub(r'\r\n', '\n', ''.join(fp.readlines()))
+        self.process_message(raw)
 
     # noinspection PyAttributeOutsideInit
-    def process_message(self, message):
-        content_type = message.get_content_type()
-        filename = message.get_filename()
-        if content_type == 'message/rfc822' or (content_type == 'text/plain'
-                                                and filename == 'spam.txt'):
-            if content_type == 'message/rfc822':
-                embedded = message.get_payload(0)
-            else:
-                embedded = message
-
+    def process_message(self, raw):
+        message = email.message_from_string(raw)
+        disposition = message['Content-Disposition']
+        if disposition and disposition.startswith('attachment'):
+            contents = extract_contents(raw)
             if message['Content-Transfer-Encoding'] == 'base64':
-                self.original_message = base64.b64decode(embedded.get_payload())
-                embedded = email.message_from_string(self.original_message)
+                contents = base64.b64decode(contents)
             elif message['Content-Transfer-Encoding'] == 'quoted-printable':
-                self.original_message = quopri.decodestring(
-                    embedded.get_payload())
-                embedded = email.message_from_string(self.original_message)
-            else:
-                # In order to extract the exact contents of the original message
-                # we must search for the content within the MIME boundaries in
-                # the file passed to the process method. The email.generator
-                # package can change the format of the message when flattening
-                # a message's payload, which will prevent a valid DKIM signature
-                # for the embedded message from being verified.
-                start = 0
-                while self.index != 0:
-                    start = self.original_message.index(self.boundary, start)
-                    start += len(self.boundary) + 1
-                    self.index -= 1
+                contents = quopri.decodestring(contents)
 
-                # Advance the end index past the opening linefeed of the
-                # boundary and extract the embedded message. The original
-                # message can then be extracted by omitting the headers and
-                # extracting the body content.
-                end = self.original_message.index(self.boundary, start)
-                rfc_message = self.original_message[start:end + 1]
-                lines = rfc_message.split('\n')
-                regexp = re.compile(r'^\s*$')
-                start = 0
-                while True:
-                    if regexp.search(lines[start]):
-                        break
-
-                    start += 1
-
-                self.original_message = '\n'.join(lines[start + 1:])
-
-            self.act_on_message(embedded)
-            self.processed = True
-        elif 'attachment' in (message['Content-Disposition'] or ''):
-            self.original_message = message.get_payload()
-            self.act_on_message(
-                email.message_from_string(self.original_message))
-            self.processed = True
+            self.act_on_message(email.message_from_string(contents))
+            sys.exit(0)
+        elif isinstance(message.get_payload(), list):
+            num_parts = len(message.get_payload())
+            boundary = '\n--' + message.get_boundary()
+            offset = 0
+            for i in range(num_parts):
+                part, offset = extract_message(raw, boundary, offset)
+                self.process_message(part)
         else:
-            payload = message.get_payload()
-            if isinstance(payload, list):
-                self.boundary = '\n--' + message.get_boundary()
-                self.index = 1
-                for p in payload:
-                    if not self.processed:
-                        self.process_message(p)
-                        self.index += 1
+            self.act_on_message(message)
+            sys.exit(0)
 
     def act_on_message(self, message):
         raise Exception('Implementation not found')
+
+
+def extract_message(message, boundary, offset):
+    # In order to extract the exact contents of the original message
+    # we must search for the content within the MIME boundaries in
+    # the file passed to the process method. The email.generator
+    # package can change the format of the message when flattening
+    # a message's payload, which will prevent a valid DKIM signature
+    # for the embedded message from being verified.
+    start = message.index(boundary, offset)
+    start += len(boundary) + 1
+
+    # Advance the end index past the opening linefeed of the
+    # boundary and extract the embedded message.
+    end = message.index(boundary, start)
+    rfc_message = message[start:end + 1]
+    return rfc_message, end
+
+
+def extract_contents(message):
+    separator = message.index('\n\n')
+    return message[separator + 2:]
